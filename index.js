37ae36df424b9211646b68b8c3bcd99d42714f9c
Stephanie Ann Solis <stephaniesolis@lambdastudents.com>
2021-04-02 19:23:02 -0600

    Task 7 returned array with names

--- index.js
+++ index.js
@@ -311,9 +311,17 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
-  /*Your Code Here*/
+function lotsOfArt(array){          //set parameters
+  const moreThanHundred = [];
+  for (let i = 0; i < array.length; i++){
+    if (array[i].paintings > 100){
+      moreThanHundred.push(array[i].name);
+    }   
+  }
+  return moreThanHundred;
 }
+
+console.log(lotsOfArt(artists));
 
 
 
